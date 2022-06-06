dd58b595fa8e532d9acb16722ac8f8d78f4ff6a4
theyashpatel <yashpatel26@gmail.com>
2022-06-06 23:03:02 +0530

    minor refactor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Developer
+            I'm a Backbend Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
             I have 8 years of experience building and desgining software.
