410bdfec67e8c4f2bedf47b39cf7199960cda9d2
theyashpatel <yashpatel26@gmail.com>
2022-06-06 19:26:28 +0530

    e

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,7 +1,67 @@
 import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const SocialLinks = () => {
-  return <div>SocialLinks</div>;
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          LinkedIn <FaLinkedin size={30} />
+        </>
+      ),
+      href: "https://linkedin.com",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          GitHub <FaGithub size={30} />
+        </>
+      ),
+      href: "https://github.com/theyashpatel",
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Mail <HiOutlineMail size={30} />
+        </>
+      ),
+      href: "mailto:foo@gmail.com",
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Resume <BsFillPersonLinesFill size={30} />
+        </>
+      ),
+      href: "/resume.pdf",
+      style: "rounded-br-md",
+      download: true,
+    },
+  ];
+
+  return (
+    <div className="flex flex-col top-[35%] left-0 fixed">
+      <ul>
+        <li className="flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500">
+          <a
+            href=""
+            className="flex justify-between items-center w-full text-white"
+          >
+            <>
+              LinkedIn <FaLinkedin size={30} />
+            </>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default SocialLinks;
