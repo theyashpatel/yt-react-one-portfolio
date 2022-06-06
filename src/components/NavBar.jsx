b5a6a2af01663e3ae4e917fcd5f7602c41a7c106
theyashpatel <yashpatel26@gmail.com>
2022-06-06 17:41:22 +0530

    b

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBar = () => {
+  return <div>NavBar</div>;
+};
+
+export default NavBar;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      signature: ["Great Vibes"],
+    },
+  },
+  plugins: [],
+};
