11ddf6d9a0c3682bd36fdaefdc42fe2c733c9788
theyashpatel <yashpatel26@gmail.com>
2022-06-06 19:31:21 +0530

    f

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -47,18 +47,28 @@ const SocialLinks = () => {
   ];
 
   return (
-    <div className="flex flex-col top-[35%] left-0 fixed">
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
       <ul>
-        <li className="flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500">
-          <a
-            href=""
-            className="flex justify-between items-center w-full text-white"
+        {links.map(({ id, child, href, style, download }) => (
+          <li
+            key={id}
+            className={
+              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
+              " " +
+              style
+            }
           >
-            <>
-              LinkedIn <FaLinkedin size={30} />
-            </>
-          </a>
-        </li>
+            <a
+              href={href}
+              className="flex justify-between items-center w-full text-white"
+              download={download}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
