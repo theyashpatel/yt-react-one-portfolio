cc961659a3ae83abd955d409452d4f145a9c3fe5
Shrey <53387208+ShreySinha02@users.noreply.github.com>
2023-03-13 15:30:26 +0530

    Update Contact.jsx
    typo

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            Cotact
+            Contact
           </p>
           <p className="py-6">Submit the form below to get in touch with me</p>
         </div>
